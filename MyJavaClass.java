1ad3574077f207e0226c24a6bdbe46c4a0552c99
johnyshaik <jbshaik82@gmail.com>
2016-02-19 22:45:38 -0500

    Practise more to learn more Practise dialy to unlease the power of git. 11

--- MyJavaClass.java
+++ MyJavaClass.java
@@ -6,6 +6,7 @@ public static void main(String args[]) {
 
 System.out.println("This is first commit to GITHUB");
 System.out.println("Practising GIT is fun.");
+System.out.println("The more you practice the more you learn.");
 
 }
 
